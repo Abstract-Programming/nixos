22bfbac5e548b11c4d691d49f26c4299cc425a7a
akat <49052296+Abstract-Programming@users.noreply.github.com>
2021-12-18 21:18:14 -0700

    Update home.nix

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -1,9 +1,23 @@
 { config, pkgs, lib, ... }: {
   programs.direnv.enable = true;
-  programs.zsh.enable = true;
-  
-  programs.starship = {
+  programs.zsh = {
     enable = true;
+    shellAliases = {
+      ll = "ls -l";
+      nupdate = "sudo nixos-rebuild switch";
+    };
+    history = {
+      size = 10000;
+      path = "${config.xdg.dataHome}/zsh/history";
+    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "agnoster";
+    };
+  };
+  programs.starship = {
+    enable = false;
     enableZshIntegration = true;
     settings = {
       character = {
@@ -13,4 +27,15 @@
       };
    };
   };
+  nixpkgs.overlays = [ (self: super: {
+  	weechat = super.weechat.override {
+    		configure = { availablePlugins, ... }: {
+      			scripts = with super.weechatScripts; [
+        			weechat-otr
+        			wee-slack
+ 	     			];
+ 	   		};
+ 	 	};
+  	}
+  )];
 }
